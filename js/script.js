7016ea8a7e78972a37fa87d04b05f0b677e8caa0
Isma <master_work@outlook.com>
2021-02-13 22:20:44 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMaried: false
+};
+console.log(persone["name"]);
+let arr = ['resmik.png', 'sratil.jpg', 'fulka.bmp'];
+console.log(arr[0]);
