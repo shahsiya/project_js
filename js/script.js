fe966f00ecc5f8f244f3e0b83e875387902c43f2
Isma <master_work@outlook.com>
2021-02-16 19:23:04 +0200

    budget

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 'use strict';
 
-let persone = {
-    name: "John",
-    age: 25,
-    isMaried: false
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
 };
-console.log(persone["name"]);
-let arr = ['resmik.png', 'sratil.jpg', 'fulka.bmp'];
-console.log(arr[0]);
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
