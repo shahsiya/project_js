19742f082e6d1d766de352782acfe4f3e63cef18
Isma <master_work@outlook.com>
2021-02-21 18:42:40 +0200

    new_js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 'use strict';
+let money, time;
 
-let money = +prompt("Ваш бюджет на месяц?", ''),
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", '');
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", '');
+    }
+}
+start();
 
 let appData = {
     budget: money,
@@ -9,24 +16,73 @@ let appData = {
     optionalExpenses: {},
     income: [],
     timeData: time,
-    savings: false
-};
+    savings: true,
+    chooseEkspenses: function() {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+                b = prompt("Во сколько обойдется?", '');
 
+            if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null &&
+                a != '' && b != '' && a.length < 50) {
+                console.log("done");
+                appData.expenses[a] = b;
+            } else {
+                console.log("bad result");
+                i--;
+            }
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        b = prompt("Во сколько обойдется?", '');
-
-    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null &&
-        a != '' && b != '' && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else {
-        console.log("bad result");
-        i--;
+        }
+    },
+    detectDayBudget: function() {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        alert("Ежедневный бюджет" + appData.moneyPerDay);
+    },
+    detectLevel: function() {
+        if (appData.moneyPerDay < 100) {
+            console.log("Минималка");
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            console.log("Среднячек");
+        } else if (appData.moneyPerDay > 2000) {
+            console.log("То что надо");
+        } else {
+            console.log("И снова ошибка");
+        }
+    },
+    checkSavings: function() {
+        if (appData.savings == true) {
+            let save = +prompt("Какова сумма накоплений?"),
+                percent = +prompt("Под какой процент?");
+            appData.monthIncome = save / 100 / 12 * percent;
+            alert("Доход в месяц с вашего депозита " + appData.monthIncome);
+        }
+    },
+    chooseOptEkspenses: function() {
+        for (let i = 1; i < 3; i++) {
+            let opt = prompt("Статья необязательных расходов?", "");
+            appData.optionalExpenses[i] = opt;
+        }
+    },
+    chooseIncome: function() {
+        for (let i = 1; i < 3; i++) {
+            let items = prompt('Что принесет дополнительный доход? (Перечислите через запятую)', '');
+            if ((typeof(items)) === 'string' && (typeof(items)) != null && items != '' && items.length < 50) {
+                console.log("done");
+            } else {
+                console.log("Введите корректные данные");
+                i--;
+            }
+            appData.income = items.split(', ');
+            appData.income.push(prompt('Может еще?'));
+            appData.income.sort();
+        }
+        appData.income.forEach(function(itemmassive, i) {
+            alert("Способы доп.заработка: " + (i + 1) + "-" + itemmassive);
+        });
     }
-
 };
+for (let key in appData) {
+    console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
+}
 
 // Используем цикл WHILE
 
@@ -70,18 +126,3 @@ for (let i = 0; i < 2; i++) {
 //     i++;
 // }
 // while(i < 2);
-
-appData.moneyPerDay = appData.budget / 30;
-
-
-
-alert("Ежедневный бюджет" + appData.moneyPerDay);
-if (appData.moneyPerDay < 100) {
-    console.log("Минималка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Среднячек");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("То что надо");
-} else {
-    console.log("И снова ошибка");
-}
